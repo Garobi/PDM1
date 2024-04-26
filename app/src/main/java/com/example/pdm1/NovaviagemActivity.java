b9fcdc86104d8695f5b8967b97b1331443a3a948
Garobi <gabriel.rbittencourt15@gmaill.com>
2024-04-25 23:25:06 -0300

    Initial commit

---
+++ app/src/main/java/com/example/pdm1/NovaviagemActivity.java
@@ -0,0 +1,25 @@
+package com.example.pdm1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NovaviagemActivity extends AppCompatActivity {
+
+    TextView detalhes;
+    LinearLayout cardlayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novaviagem);
+        detalhes = findViewById(R.id.detalhes);
+        cardlayout = findViewById(R.id.cardLayout);
+    }
+
+    public void expand(View view) {
+        int v (detalhes.)
+    }
+}
